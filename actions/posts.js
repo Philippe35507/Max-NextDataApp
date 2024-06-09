8fc4e1deab83f5cbb7657215fa05dac1b5a46d56
Philippe <55062877+Philippe35507@users.noreply.github.com>
2024-06-09 12:41:53 +0000

    RevalidatePath: Revalidating Data to avoid Caching Problems
    Change is reflected after 3 seconds

--- actions/posts.js
+++ actions/posts.js
@@ -3,6 +3,7 @@
 import { redirect } from "next/navigation";
 import { storePost, updatePostLikeStatus } from "@/lib/posts";
 import { uploadImage } from "@/lib/cloudinary";
+import { revalidatePath } from "next/cache";
 
 export async function createPost(prevState, formData) {
   const title = formData.get("title");
@@ -46,6 +47,7 @@ export async function createPost(prevState, formData) {
   redirect("/feed");
 }
 
-export async function togglePostLikeStatus(postId){
-  updatePostLikeStatus(postId, 2)
+export async function togglePostLikeStatus(postId) {
+  await updatePostLikeStatus(postId, 2);
+  revalidatePath("/", "layout");
 }
