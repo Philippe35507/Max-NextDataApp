8917dda2d3074a7af38e2be60df7efc79124b0b5
Philippe <55062877+Philippe35507@users.noreply.github.com>
2024-06-09 10:22:55 +0000

    Adjusting Server Actions for useFormState part1: creating a separate client component
    One can't define a server action in a client component but it is allowed to pass a server action from a server to a client component throught props {action}.

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,7 +1,4 @@
-"use client";
-
-import { useFormState } from "react-dom";
-import FormSubmit from "@/components/form-submit";
+import PostForm from "@/components/post-form";
 import { storePost } from "@/lib/posts";
 import { redirect } from "next/navigation";
 
@@ -39,33 +36,5 @@ export default function NewPostPage() {
     redirect("/feed");
   }
 
-  const [state, formAction] = useFormState(createPost, {});
-
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={formAction}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-          <FormSubmit />
-        </p>
-      </form>
-    </>
-  );
+  return <PostForm action={createPost} />;
 }
